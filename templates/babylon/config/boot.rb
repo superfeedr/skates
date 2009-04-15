83dcd30e6923a9ed1ada8918cdf7eccdb8ae35cf
julien <julien.genestoux@gmail.com>
2009-04-15 11:53:01 -0700

    refactoring initializers

--- templates/babylon/config/boot.rb
+++ templates/babylon/config/boot.rb
@@ -12,5 +12,5 @@ require File.dirname(__FILE__) + "/dependencies"
 # Start the App
 Babylon::Runner::run(ARGV[0] || "development") do
   # Run the initializers, too. This is done here since some initializers might need EventMachine to be started.
-  Dir[File.join(File.dirname(__FILE__), '/initializers/*.rb')].each {|f| require f }
+  Dir.glob('initializers/*.rb').each { |f| require f }
 end
