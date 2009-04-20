37acacfa9e5479bd943a48a4134894a74bddbb46
julien <julien.genestoux@gmail.com>
2009-04-19 20:58:07 -0700

    adding spec for generator

---
+++ spec/lib/babylon/generator_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Babylon::Generator::ApplicationGenerator do
+  # Hum, how do we spec Templater?
+end
+
+
+describe Babylon::Generator::ControllerGenerator do
+  # Hum, how do we spec Templater?
+end
