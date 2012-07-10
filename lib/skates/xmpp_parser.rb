df3b392b3672fe04abf4d90046b19c3b8ccaaa61
julien <julien.genestoux@gmail.com>
2012-07-10 11:52:02 +0200

    fixing the namespacing

--- lib/skates/xmpp_parser.rb
+++ lib/skates/xmpp_parser.rb
@@ -3,9 +3,9 @@ module Skates
   ##
   # This is the XML SAX Parser that accepts "pushed" content
   class XmppParser < Nokogiri::XML::SAX::Document
-    
+
     attr_accessor :elem, :doc, :parser
-    
+
     ##
     # Initialize the parser and adds the callback that will be called upon stanza completion
     def initialize(callback)
@@ -14,27 +14,27 @@ module Skates
       super()
       reset
     end
-    
-    ## 
+
+    ##
     # Resets the Pushed SAX Parser.
     def reset
       @parser = Nokogiri::XML::SAX::PushParser.new(self, "UTF-8")
       @elem = @doc = nil
     end
-    
+
     ##
     # Pushes the received data to the parser. The parser will then callback the document's methods (start_tag, end_tag... etc)
     def push(data)
       @parser << data
     end
-    
+
     ##
     # Adds characters to the current element (being parsed)
     def characters(string)
       @buffer ||= ""
-      @buffer << string 
+      @buffer << string
     end
-    
+
     ##
     # Instantiate a new current Element, adds the corresponding attributes and namespaces.
     # The new element is eventually added to a parent element (if present).
@@ -44,33 +44,36 @@ module Skates
       @doc  ||= Nokogiri::XML::Document.new
       @elem ||= @doc # If we have no current element, then, we take the doc
       @parent = @elem
-      
+
       # First thing first : identify namespaces, and attributes
       namespaces, attributes = identify_namespaces_and_attributes(attributes)
-      
+      @parent.namespaces.each do |p, u|
+        namespaces[p.split(":")[1]] ||= u
+      end
+
       # Now, we may need to add a namespace!
       namespace, qname = qname.split(":") if qname.match(/.*:.*/)
-      
+
       @elem = Nokogiri::XML::Element.new(qname, @doc)
       # Let's now add all namespaces
       namespaces.each do |prefix, uri|
         set_namespace(prefix, uri)
       end
-      
+
       # And all attributes
       attributes.each do |name, value|
         set_attribute(name, value)
       end
-      
+
       # And now addback the namespace if applicable
-      if prefix = ["xmlns", namespace].join(":") and uri = @parent.namespaces[prefix] 
-        @elem.namespace = @parent.namespace_definitions.select() {|ns|
-          ns.href == uri
+      if namespace
+        @elem.namespace = @elem.namespace_definitions.select() {|ns|
+          ns.prefix == namespace
         }.first
       end
-      
+
       @parent.add_child(@elem) # Finally, add the element
-      
+
       if @elem.name == "stream"
         # We activate the callback since this element  will never end.
         @callback.call(@elem)
@@ -78,7 +81,7 @@ module Skates
         # And then, we start a new Sax Push Parser
       end
     end
-    
+
     ##
     # Clears the characters buffer
     def clear_characters_buffer
@@ -96,17 +99,17 @@ module Skates
       if @elem
         if @elem.parent == @doc
           # If we're actually finishing the stanza (a stanza is always a document's root)
-          @callback.call(@elem) 
+          @callback.call(@elem)
           # We delete the current element and the doc (1 doc per stanza policy)
-          @elem = @doc = nil 
+          @elem = @doc = nil
         else
-          @elem = @elem.parent 
-        end 
-      else 
+          @elem = @elem.parent
+        end
+      else
         # Not sure what to do since it seems we're not processing any element at this time, so how can one end?
-      end 
-    end 
-    
+      end
+    end
+
     ##
     # Idenitifies namespaces and attributes
     # Nokogiri passes them as a array of [[ns_name, ns_url], [ns_name, ns_url]..., key, value, key, value]...
@@ -127,13 +130,13 @@ module Skates
       end
       [namespaces, attributes]
     end
-    
+
     def set_attribute(key, value)
       @elem.set_attribute(key, Skates.decode_xml(value))
     end
-    
+
     def set_namespace(key, value)
       @elem.add_namespace(key, value)
     end
-  end 
-end 
+  end
+end
--- spec/lib/skates/xmpp_parser_spec.rb
+++ spec/lib/skates/xmpp_parser_spec.rb
@@ -20,7 +20,7 @@ describe Skates::XmppParser do
       @parser.reset
       @parser.elem.should be_nil
     end
-    
+
     it "should reset the parser to a parser" do
       new_parser = Nokogiri::XML::SAX::PushParser.new(@parser, "UTF-8")
       Nokogiri::XML::SAX::PushParser.should_receive(:new).with(@parser, "UTF-8").and_return(new_parser)
@@ -28,7 +28,7 @@ describe Skates::XmppParser do
       @parser.parser.should == new_parser
     end
   end
-  
+
   describe ".push" do
     it "should send the data to the parser" do
       data = "<name>me</name>"
@@ -36,19 +36,19 @@ describe Skates::XmppParser do
       @parser.push(data)
     end
   end
-  
+
   describe ".characters" do
-    before(:each) do 
+    before(:each) do
       @parser.doc = Nokogiri::XML::Document.new
       @parser.elem = Nokogiri::XML::Element.new("element", @parser.doc)
     end
-    
+
     it "should add the characters to the buffer" do
       chars = "hello my name is julien"
       @parser.characters(chars)
       @parser.instance_variable_get("@buffer").should == chars
     end
-    
+
     it "should concatenate the text if we receive in both pieces" do
       chars = "hello my name is julien"
       @parser.characters(chars)
@@ -66,14 +66,14 @@ describe Skates::XmppParser do
       (@parser.elem / 'message/body').to_xml(:encoding => "UTF-8").should == "<body>&amp;#187;</body>"
     end
   end
-  
+
   describe ".start_element" do
-    
+
     before(:each) do
       @new_elem_name = "new"
       @new_elem_attributes = [["to", "you@yourserver.com/home"], ["xmlns", "http://ns.com"]]
     end
-    
+
     it "should create a new doc if we don't have one" do
       new_doc = Nokogiri::XML::Document.new
       @parser.doc = nil
@@ -81,13 +81,13 @@ describe Skates::XmppParser do
       @parser.start_element(@new_elem_name, @new_elem_attributes)
       @parser.doc.should == new_doc
     end
-    
+
     it "should not create a new doc we already have one" do
       @parser.doc = Nokogiri::XML::Document.new
       Nokogiri::XML::Document.should_not_receive(:new)
       @parser.start_element(@new_elem_name, @new_elem_attributes)
     end
-    
+
     it "should create a new element" do
       @doc = Nokogiri::XML::Document.new
       @parser.doc = @doc
@@ -95,7 +95,7 @@ describe Skates::XmppParser do
       Nokogiri::XML::Element.should_receive(:new).and_return(@new_elem)
       @parser.start_element(@new_elem_name, @new_elem_attributes)
     end
-    
+
     it "should add the new element as a child to the current element if there is one" do
       @doc = Nokogiri::XML::Document.new
       @parser.doc = @doc
@@ -106,28 +106,28 @@ describe Skates::XmppParser do
       @parser.start_element(@new_elem_name, @new_elem_attributes)
       @new_elem.parent.should == @current
     end
-    
+
     it "should add the new element as the child of the doc if there is none" do
       @doc = Nokogiri::XML::Document.new
       @parser.doc = @doc
       @new_elem = Nokogiri::XML::Element.new(@new_elem_name, @parser.doc)
       Nokogiri::XML::Element.stub!(:new).and_return(@new_elem)
       @parser.start_element(@new_elem_name, @new_elem_attributes)
-      @new_elem.parent.should == @doc      
+      @new_elem.parent.should == @doc
     end
-    
+
     it "should add the right attributes and namespaces to the newly created element" do
       @parser.start_element(@new_elem_name, @new_elem_attributes)
       @parser.elem["to"].should == "you@yourserver.com/home"
       @parser.elem.namespaces.should == {"xmlns"=>"http://ns.com"}
     end
-    
+
     describe "when the new element is of name stream:stream" do
       it "should callback" do
         @proc.should_receive(:call)
         @parser.start_element("stream:stream", [])
       end
-      
+
       it "should reinit to nil the doc and the elem" do
         @parser.start_element("stream:stream", [])
         @parser.doc.should == nil
@@ -143,7 +143,7 @@ describe Skates::XmppParser do
       @current = Nokogiri::XML::Element.new("element", @parser.doc)
       @parser.elem = @current
     end
-    
+
     it "should add the content of the buffer to the @elem" do
       @elem = Nokogiri::XML::Element.new("element", @parser.doc)
       chars = "hello world"
@@ -152,54 +152,54 @@ describe Skates::XmppParser do
       @parser.end_element("element")
       @elem.content.should == chars
     end
-    
+
     describe "when we're finishing the doc's root" do
       before(:each) do
         @parser.doc.root = @current
       end
-      
+
       it "should callback" do
         @proc.should_receive(:call)
         @parser.end_element("element")
       end
-      
+
       it "should reinit to nil the doc and the elem" do
         @parser.end_element("element")
         @parser.doc.should == nil
         @parser.elem.should == nil
       end
     end
-    
+
     describe "when we're finishing another element" do
       before(:each) do
         @parser.doc.root = Nokogiri::XML::Element.new("root", @parser.doc)
         @current.parent = @parser.doc.root
       end
-      
+
       it "should go back up one level" do
         @parser.end_element("element")
         @parser.elem = @current.parent
       end
     end
   end
-  
+
   describe "a communication with an XMPP Client" do
-    
+
     before(:each) do
       @stanzas = []
       @proc = Proc.new { |stanza|
-        @stanzas << stanza 
+        @stanzas << stanza
       }
       @parser = Skates::XmppParser.new(@proc)
     end
-      
+
     it "should parse the right information" do
       string = "<stream:stream
           xmlns:stream='http://etherx.jabber.org/streams'
           xmlns='jabber:component:accept'
           from='plays.shakespeare.lit'
           id='3BF96D32'>
-          <handshake/>    
+          <handshake/>
           <message from='juliet@example.com'
                         to='romeo@example.net'
                         xml:lang='en'>
@@ -223,17 +223,17 @@ describe Skates::XmppParser do
       substrings << string[prev_index, string.size]
       #Just to make sure we split the string the right way!
       substrings.join("").should == string
-      
+
       substrings.each do |s|
         @parser.push(s)
       end
-      
+
       @stanzas.map(&:to_xml).join("").should == "<stream xmlns:stream=\"http://etherx.jabber.org/streams\" xmlns=\"jabber:component:accept\" from=\"plays.shakespeare.lit\" id=\"3BF96D32\"/><handshake/><message from=\"juliet@example.com\" to=\"romeo@example.net\" xml:lang=\"en\">\n  <body>Art thou not Romeo, and a Montague?</body>\n  <link href=\"http://sfbay.craigslist.org/search/sss?query=%2522mac+mini%2522+Intel+Core+Duo&amp;minAsk=min&amp;maxAsk=max&amp;format=rss&amp;format=rss\"/>\n</message>"
       @stanzas.last.at("link")["href"].should == "http://sfbay.craigslist.org/search/sss?query=%2522mac+mini%2522+Intel+Core+Duo&minAsk=min&maxAsk=max&format=rss&format=rss"
     end
-    
+
   end
-  
+
   describe "when parsing a complex stanza" do
     before(:each) do
       @xml =<<-EOXML
@@ -455,19 +455,19 @@ describe Skates::XmppParser do
   </pubsub>
 </iq>
 EOXML
-      
-      @proc = Proc.new { |stanza|  
+
+      @proc = Proc.new { |stanza|
         stanza.to_xml(:encoding => "UTF-8").should == @xml.strip
       }
-      @parser = Skates::XmppParser.new(@proc) 
+      @parser = Skates::XmppParser.new(@proc)
     end
-    
+
     it "should parse correctly the namespaces and the attributes" do
       @parser.push(@xml)
     end
   end
-  
-  
+
+
   describe "when parsing a complex stanza" do
     before(:each) do
       @xml =<<-EOXML
@@ -498,7 +498,7 @@ EOXML
 
 
         &lt;p&gt;Si vous êtes interessé, n'hésitez pas à contacter Caroline Olive, journaliste TV Presse pour "90mn enquêtes" au 06 87 66 61 13.&lt;/p&gt;</content>
-          <link rel="alternate" type="text/html" href="http://www.jobetudiant.net/blog/index.php?2011/04/29/573-appel-tmoins-pour-mission-de-tv-sur-les-jobs-d-t" title="Appel à témoins pour émission de TV sur les jobs d'été"/>
+          <link rel="alternate" type="text/html" href="http://www.jobetudiant.net/blog/index.php?2011/04/29/573-appel-tmoins-pour-mission-de-tv-sur-les-jobs-d-t" title="Appel &#xE0; t&#xE9;moins pour &#xE9;mission de TV sur les jobs d'&#xE9;t&#xE9;"/>
         </entry>
       </item>
     </publish>
@@ -510,15 +510,106 @@ EOXML
 
 EOXML
     end
-    
+
     it "should parse correctly the namespaces and the attributes" do
-      @proc = Proc.new { |stanza|  
+      @proc = Proc.new { |stanza|
         stanza.to_xml(:encoding => "UTF-8").should == @xml.strip
       }
-      @parser = Skates::XmppParser.new(@proc) 
+      @parser = Skates::XmppParser.new(@proc)
       @parser.push(@xml)
     end
-    
+
   end
-  
+
+  describe "when parsing a complex stanza" do
+    before(:each) do
+      @xml =<<-EOXML
+<iq to="test-track.superfeedr.com" id="pub-284" type="set">
+  <feed xmlns="http://superfeedr.com/xmpp-superfeedr-ext" id="16835658">
+    <url>https://api.flattr.com/rest/v2/users/voxpelli/activities.atom</url>
+    <http_code>200</http_code>
+    <last_modified>2012-07-08T18:41:13+02:00</last_modified>
+    <etag/>
+    <number_of_new_entries>8</number_of_new_entries>
+    <last_error_message>30666B in 1.214204s, 8/8 new entries</last_error_message>
+    <last_fetch>2012-07-08T18:41:13+02:00</last_fetch>
+    <next_fetch>2012-07-09T06:23:11+02:00</next_fetch>
+    <period>43200</period>
+    <last_maintenance_at>2012-07-08T18:40:25+02:00</last_maintenance_at>
+    <entries_count>63</entries_count>
+    <perform_maintenance/>
+    <title>Activities by VoxPelli</title>
+    <format>feed</format>
+    <link href="https://flattr.com/profile/voxpelli" rel="alternate" type="text/html"/>
+    <link href="https://api.flattr.com/rest/v2/users/voxpelli/activities.atom" rel="self" type="application/atom+xml"/>
+    <link href="http://flattr.superfeedr.com" rel="hub" type="text/html"/>
+    <last_parse>2012-07-08T18:41:14+02:00</last_parse>
+    <headers>
+      <server>nginx/0.7.67</server>
+      <date>Sun, 08 Jul 2012 16:41:13 GMT</date>
+      <content_type>application/atom+xml</content_type>
+      <transfer_encoding>chunked</transfer_encoding>
+      <connection>close</connection>
+      <access_control_allow_origin>*</access_control_allow_origin>
+      <access_control_allow_headers>content-length, authorization</access_control_allow_headers>
+      <access_control_allow_methods>GET, PUT, POST, DELETE, PATCH</access_control_allow_methods>
+      <expires>Wed, 7 Jul 1982 13:37:00 GMT</expires>
+      <last_modified>Sun, 8 Jul 2012 16:41:13 GMT</last_modified>
+      <x_ratelimit_limit>1000</x_ratelimit_limit>
+      <x_ratelimit_remaining>970</x_ratelimit_remaining>
+      <x_ratelimit_current>30</x_ratelimit_current>
+      <x_ratelimit_reset>1341767504</x_ratelimit_reset>
+      <content_encoding>gzip</content_encoding>
+      <vary>Accept-Encoding</vary>
+    </headers>
+    <id>tag:flattr.com,2010-02:users:voxpelli:activities</id>
+  </feed>
+  <pubsub xmlns="http://jabber.org/protocol/pubsub">
+    <publish node="16835658">
+      <item>
+        <entry xmlns="http://www.w3.org/2005/Atom" xmlns:geo="http://www.georss.org/georss" xmlns:as="http://activitystrea.ms/spec/1.0/" xmlns:sf="http://superfeedr.com/xmpp-pubsub-ext" xml:lang="fr">
+          <id>tag:flattr.com,2012:things/734695</id>
+          <published>2012-07-08T18:33:00+02:00</published>
+          <updated>2012-07-08T18:33:00+02:00</updated>
+          <title>VoxPelli got a new thing, "Tweet by @voxpelli, 6 Jul"</title>
+          <summary type="text"/>
+          <link rel="alternate" type="text/html" href="https://flattr.com/thing/734695/Tweet-by-voxpelli-6-Jul" title="VoxPelli got a new thing, &quot;Tweet by @voxpelli, 6 Jul&quot;"/>
+          <link rel="payment" type="text/html" href="https://flattr.com/submit/auto?url=https%3A%2F%2Ftwitter.com%2Fvoxpelli%2Fstatus%2F221273557588652032" title="Flattr this!"/>
+          <author>
+            <name>VoxPelli</name>
+            <uri>https://flattr.com/profile/VoxPelli</uri>
+            <email/>
+            <as:object-type>person</as:object-type>
+            <link rel="alternate" type="text/html" href="https://flattr.com/profile/VoxPelli" title=""/>
+          </author>
+          <as:verb>post</as:verb>
+          <as:object>
+            <as:object-type>bookmark</as:object-type>
+            <id>tag:flattr.com,2012:things/734695</id>
+            <title>Tweet by @voxpelli, 6 Jul</title>
+            <published>2012-07-08T18:33:00+02:00</published>
+            <updated>2012-07-08T18:33:00+02:00</updated>
+            <content type="text"/>
+            <link rel="alternate" type="text/html" href="https://flattr.com/thing/734695/Tweet-by-voxpelli-6-Jul" title="Tweet by @voxpelli, 6 Jul"/>
+            <link rel="related" type="text/html" href="https://twitter.com/voxpelli/status/221273557588652032" title="Tweet by @voxpelli, 6 Jul"/>
+          </as:object>
+        </entry>
+      </item>
+    </publish>
+  </pubsub>
+</iq>
+EOXML
+    end
+
+    it "should parse correctly the namespaces and the attributes" do
+      @proc = Proc.new { |stanza|
+        stanza.to_xml(:encoding => "UTF-8").should == @xml.strip
+      }
+      @parser = Skates::XmppParser.new(@proc)
+      @parser.push(@xml)
+    end
+
+  end
+
+
 end
